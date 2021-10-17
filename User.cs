a51a7e9d125726c856d1c4c8a55e13d140e278cb
hbkdshamal <64688853+dilukshashamal@users.noreply.github.com>
2021-10-17 07:11:05 +0530

    Add files via upload
    add management system

---
+++ User.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nsbm_University_Management_System
+{
+    class User
+    {
+    
+
+        DbCon con = new DbCon();
+
+        public string Uname;
+        public string Passwrd;
+
+        public bool Login()
+        {
+            String Query = "SELECT * FROM `user` WHERE `username`= @usr  AND `password`= @pass";
+            MySqlCommand Command = new MySqlCommand(Query, con.Getconnection);
+            Command.Parameters.Add("@usr", MySqlDbType.VarChar).Value = Uname;
+            Command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = Passwrd;
+            MySqlDataAdapter Adptr = new MySqlDataAdapter(Command);
+            DataTable Tbl = new DataTable();
+            Adptr.Fill(Tbl);
+            if (Tbl.Rows.Count > 0)
+            {
+                return true;
+            }
+                return false;
+        }
+    }
+}
